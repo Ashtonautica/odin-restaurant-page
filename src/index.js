21708893babb381bb0bf9ea65e63f2dd1ddb7579
Ashtonaut <ashtonautica@gmail.com>
2024-02-29 20:11:46 +0000

    Complete assignment step 5

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('Test');
+import loadPage from "./loadPage";
+
+loadPage();
---
+++ src/loadPage.js
@@ -0,0 +1,17 @@
+import pizzaTime from './pizzatime.jpg';
+
+export default () => {
+    const contentDiv = document.querySelector('#content');
+
+    const header = document.createElement('h1');
+    header.innerHTML = "Joe's Pizza";
+    contentDiv.appendChild(header);
+
+    const image = document.createElement('img');
+    image.src = pizzaTime;
+    contentDiv.appendChild(image);
+
+    const para = document.createElement('p');
+    para.innerHTML = "Pizza Time!";
+    contentDiv.appendChild(para);
+};
