e1e1c34bbf1076face15e040eb547e3b97ed01cf
Ashtonaut <ashtonautica@gmail.com>
2024-02-29 21:12:08 +0000

    Add button functionality

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@ import home from "./home";
 import menu from "./menu";
 import contact from './contact';
 
+const contentLoaders = {
+    home,
+    menu,
+    contact,
+};
 const contentDiv = document.querySelector('#content');
-contact(contentDiv);
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        const buttonLabel = e.target.innerHTML.toLowerCase();
+        contentDiv.innerHTML = "";
+        contentLoaders[buttonLabel](contentDiv);
+    });
+});
+
+contentLoaders.home(contentDiv);
