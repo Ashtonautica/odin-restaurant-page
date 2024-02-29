65d334f0bd26e1bff0155f30114533229e1f2b20
Ashtonaut <ashtonautica@gmail.com>
2024-02-29 21:16:28 +0000

    Initial dist subtree commit

--- src/contact.js
+++
@@ -1,5 +0,0 @@
-export default (contentDiv) => {
-    const p = document.createElement('p');
-    p.innerHTML = 'Pizza Time?<br>7 Carmine Street<br>New York, NY 10014<br>(212) 366-1182'
-    contentDiv.appendChild(p);
-}
